05a1852a51fdba311502d5b439c328bb178651b9
jordanandrewww <158337618+jordanandrewww@users.noreply.github.com>
2025-08-28 13:24:58 -0400

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,12 +1,12 @@
 from hello import say_hello, add
 
-
+# Checks that the appropriate hello message is printed for Annie
 def test_say_hello():
     assert (
         say_hello("Annie")
         == "Hello, Annie, welcome to Data Engineering Systems (IDS 706)!"
     )
 
-
+# adds 2 and 3 together and checks if it equals 5
 def test_add():
     assert add(2, 3) == 5
